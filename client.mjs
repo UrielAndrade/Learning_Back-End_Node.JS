e7583195d33cda8e1ff39db9c09e1bce0f35067d
UrielAndrade <andradeleiru@gmail.com>
2025-12-25 11:51:58 -0400

    #

--- client.mjs
+++ client.mjs
@@ -1,14 +1,15 @@
 const response = await fetch(
-  'http://localhost:3000/produtos?cor=azul&tamanho=g',
-  {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
+    "http://localhost:3000/produtos",
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username: "uriel", password: "1234" }),
     },
-    body: JSON.stringify({ username: 'leticia', password: '2009Lele' }),
-  },
 );
 
-const body = await response.text();
+const body = await response.json();
+console.log(body);
 
-console.log(body, '\nSatus:', response.status);
+// console.log(response);
