5b296d941204f8cfa7db7e09b0bb53ba9a53f2ff
UrielAndrade <andradeleiru@gmail.com>
2026-01-04 21:54:29 -0400

    #

--- client.mjs
+++ client.mjs
@@ -1,15 +1,12 @@
 const response = await fetch(
-    "http://localhost:3000/produtos",
-    {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username: "uriel", password: "1234" }),
-    },
-);
+    "http://localhost:3000/pizza", {
+    method: "GET",
+    // headers: {
+    //     "Content-Type": "application/json",
+    // },
+    // body: JSON.stringify({ username: "uriel", password: "1234" }),
+});
 
-const body = await response.json();
+const body = await response.text();
+// console.log(response)
 console.log(body);
-
-// console.log(response);
