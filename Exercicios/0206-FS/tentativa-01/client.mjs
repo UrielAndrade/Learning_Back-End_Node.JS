77cd7ca3d3f0d6df95277f5ec2454c2d3981316b
UrielAndrade <andradeleiru@gmail.com>
2026-01-20 14:22:33 -0400

    mandando dados

--- Exercicios/0206-FS/tentativa-01/client.mjs
+++ Exercicios/0206-FS/tentativa-01/client.mjs
@@ -1,16 +1,49 @@
-const response = await fetch(
-    "http://localhost:3000/produtos", {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'aplication/json',
-    },
-    body: JSON.stringify({
-        nome: "Notebook",
-        slug: "notebook",
-        categoria: "eletronicos",
-        preco: 4499,
-    }),
-});
-const body = await response.text();
-console.log(body);
-console.log(response);
+const produtosRes = await fetch("http://localhost:3000/produtos");
+console.log(produtosRes);
+const produtos = await produtosRes.json();
+console.log(produtos);
+
+// const response = await fetch(
+//     "http://localhost:3000/produtos", {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'aplication/json',
+//     },
+//     body: JSON.stringify({
+//         nome: "Notebook",
+//         slug: "notebook",
+//         categoria: "eletronicos",
+//         preco: 4499,
+//     }),
+// });
+// const body = await response.text();
+// console.log(body);
+// console.log(response);
+
+// await fetch(
+//     "http://localhost:3000/produtos", {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'aplication/json',
+//     },
+//     body: JSON.stringify({
+//         nome: "Mesa 4 pernas",
+//         slug: "mesa4pernas",
+//         categoria: "moveis",
+//         preco: 4499,
+//     }),
+// });
+
+// await fetch(
+//     "http://localhost:3000/produtos", {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'aplication/json',
+//     },
+//     body: JSON.stringify({
+//         nome: "mouse",
+//         slug: "mouse",
+//         categoria: "eletronicos",
+//         preco: 200,
+//     }),
+// });
